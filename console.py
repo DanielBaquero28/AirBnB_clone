74494805dbe1b99b43039da1e0e78106a9b2fd7f
Skillhh <ortegon.giovanny@hotmail.com>
2019-11-09 17:27:40 -0500

    [UPDATE] comments and sort code

--- console.py
+++ console.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python3
+""" Imports """
 import cmd
 
+
 class HBNBCommand(cmd.Cmd):
+    """ Class HBNBCommand """
     prompt = "(hbnb) "
 
     def do_EOF(self, line):
-        """ EOF exit of the program
-        with Ctrl+D """
+        """EOF command to exit the program Ctrl+D
+        """
         return True
 
     def do_quit(self, line):
         """Quit command to exit the program
-
         """
         return True
-    
+
     def emptyline(self):
+        """emptyline do not execute before command"""
         pass
 
 
