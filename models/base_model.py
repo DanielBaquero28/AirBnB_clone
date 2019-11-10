e868ca00d499429b43d99b5ecfb8b988e0910992
Skillhh <ortegon.giovanny@hotmail.com>
2019-11-09 20:47:18 -0500

    [UPDATE]

--- models/base_model.py
+++ models/base_model.py
@@ -14,7 +14,7 @@ class BaseModel:
         if kwargs is not None:
             for key, value in kwargs.items():
                 if key != '__class__':
-                    if key is 'created_at' or key is 'updated_at':
+                    if key is 'created_at' or key is 'updated_at': 
                         value = datetime.strptime(
                             value, '%Y-%m-%dT%H:%M:%S.%f')
                     setattr(self, key, value)
