b7293385241d9322229ce30ed28fa00733b21797
Skillhh <ortegon.giovanny@hotmail.com>
2019-11-12 19:44:04 -0500

    [ADD] more classes

---
+++ models/amenity.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Imports """
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """ class Amenity """
+    name = ""
---
+++ models/user.py
@@ -0,0 +1,11 @@
+#!/usr/bin/ython3
+""" Imports """
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """ Class User """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
